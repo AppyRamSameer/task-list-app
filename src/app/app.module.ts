c5738ef0a72850567b3f5aef4e96cea0f86209a3
rsameer-symplr <rsameer@symplr.com>
2023-08-04 17:24:04 +0530

    create calender component and added a angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarModule } from './calendar/calendar.module';
 import { TaskListModule } from './task-list/task-list.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TaskListModule],
+  imports: [BrowserModule, AppRoutingModule, TaskListModule, CalendarModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
